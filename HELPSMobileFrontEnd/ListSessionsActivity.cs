19bcfee3dc3c17d3f98d2839afab157bed531d98
rohan <unknown-user@fe80::b102:367a:2451:62e%16>
2015-08-24 13:03:21 +1000

    Added ListSessionsActivity Added LoginActivity Added ListSessions Layout

---
+++ HELPSMobileFrontEnd/ListSessionsActivity.cs
@@ -0,0 +1,28 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace HELPSMobileFrontEnd
+{
+	[Activity (Label = "ListSessionsActivity")]			
+	public class ListSessionsActivity : Activity
+	{
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+
+			SetContentView (Resource.Layout.Main);
+			// Create your application here
+		}
+	}
+}
+
