bad215d62fa1ff426855aefb069afa22972ff89f
rohan <unknown-user@fe80::b102:367a:2451:62e%17>
2015-08-25 18:39:24 +1000

    Added Login, made other changes

--- HELPSMobileFrontEnd/LoginActivity.cs
+++ HELPSMobileFrontEnd/LoginActivity.cs
@@ -1,8 +1,10 @@
-﻿
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 using Android.App;
 using Android.Content;
@@ -18,9 +20,52 @@ namespace HELPSMobileFrontEnd
 	{
 		protected override void OnCreate (Bundle bundle)
 		{
-			base.OnCreate (bundle);
+			try
+			{
+				base.OnCreate (bundle);
 
-			// Create your application here
+				// Create your application here
+			}
+			catch (Exception e)
+			{
+				new AlertDialog.Builder (this)
+					.SetMessage(e.Message)
+					.SetTitle("Application Error")
+					.Show();
+			}
+		}
+
+		private async Task<SLIMSMobile_Handshake> MessageFixed(SLIMSMobile_Handshake _SM_Handshake)
+		{
+			try
+			{
+
+				using (HttpClient _HttpClient = new HttpClient())
+				{
+					_HttpClient.Timeout = new TimeSpan(0, 0, 1, 0);
+					using (HttpRequestMessage _HttpRequest = new HttpRequestMessage(HttpMethod.Get, SYNC_URL + "MessageFixed"))
+					{
+						_HttpRequest.Content = new StringContent(HandshakeToString(_SM_Handshake), Encoding.UTF8, "application/json");
+						using (HttpResponseMessage _HttpResponse = await _HttpClient.SendAsync(_HttpRequest))
+						{
+							if (_HttpResponse.IsSuccessStatusCode)
+							{
+								SLIMSMobile_Handshake _SM_Handshake_Response = StringToHandshake(await _HttpResponse.Content.ReadAsStringAsync());
+								return _SM_Handshake_Response;
+							}
+							else
+							{
+								return null;
+							}
+						}
+					}
+				}
+
+			}
+			catch
+			{
+				throw;
+			}
 		}
 	}
 }
--- HELPSMobileFrontEnd/Resources/Resource.designer.cs
+++ HELPSMobileFrontEnd/Resources/Resource.designer.cs
@@ -60,20 +60,35 @@ namespace HELPSMobileFrontEnd
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f050001
-			public const int btnGetData = 2131034113;
+			// aapt resource value: 0x7f050005
+			public const int button1 = 2131034117;
+			
+			// aapt resource value: 0x7f050006
+			public const int button2 = 2131034118;
+			
+			// aapt resource value: 0x7f050008
+			public const int button3 = 2131034120;
+			
+			// aapt resource value: 0x7f050009
+			public const int button4 = 2131034121;
 			
 			// aapt resource value: 0x7f050003
-			public const int btnNavigateClassList = 2131034115;
-			
-			// aapt resource value: 0x7f050002
-			public const int lblData = 2131034114;
+			public const int frameLayout1 = 2131034115;
 			
 			// aapt resource value: 0x7f050000
 			public const int lvClasses = 2131034112;
 			
+			// aapt resource value: 0x7f050001
+			public const int tableLayout1 = 2131034113;
+			
+			// aapt resource value: 0x7f050002
+			public const int tableRow1 = 2131034114;
+			
 			// aapt resource value: 0x7f050004
-			public const int lvPageLayout = 2131034116;
+			public const int tableRow2 = 2131034116;
+			
+			// aapt resource value: 0x7f050007
+			public const int tableRow3 = 2131034119;
 			
 			static Id()
 			{
