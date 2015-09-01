9b23c70d58246d4ef10f8bb4de42cd803c63601c
rohan12111 <rohan12111@hotmail.com>
2015-09-01 21:20:35 +1000

    ojadbjadbojabdgojbadg

--- HELPSMobileFrontEnd/Resources/Resource.designer.cs
+++ HELPSMobileFrontEnd/Resources/Resource.designer.cs
@@ -47,6 +47,9 @@ namespace HELPSMobileFrontEnd
 			// aapt resource value: 0x7f020000
 			public const int Icon = 2130837504;
 			
+			// aapt resource value: 0x7f020001
+			public const int monoandroidsplash = 2130837505;
+			
 			static Drawable()
 			{
 				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
@@ -60,107 +63,107 @@ namespace HELPSMobileFrontEnd
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f050001
-			public const int bookinglist = 2131034113;
+			// aapt resource value: 0x7f060001
+			public const int bookinglist = 2131099649;
 			
-			// aapt resource value: 0x7f050000
-			public const int btnCount = 2131034112;
+			// aapt resource value: 0x7f060000
+			public const int btnCount = 2131099648;
 			
-			// aapt resource value: 0x7f05001e
-			public const int btnHelp = 2131034142;
+			// aapt resource value: 0x7f06001e
+			public const int btnHelp = 2131099678;
 			
-			// aapt resource value: 0x7f05000b
-			public const int btnLogin = 2131034123;
+			// aapt resource value: 0x7f06000b
+			public const int btnLogin = 2131099659;
 			
-			// aapt resource value: 0x7f050021
-			public const int btnLogout = 2131034145;
+			// aapt resource value: 0x7f060021
+			public const int btnLogout = 2131099681;
 			
-			// aapt resource value: 0x7f05001a
-			public const int btnMakeBooking = 2131034138;
+			// aapt resource value: 0x7f06001a
+			public const int btnMakeBooking = 2131099674;
 			
-			// aapt resource value: 0x7f050020
-			public const int btnProfile = 2131034144;
+			// aapt resource value: 0x7f060020
+			public const int btnProfile = 2131099680;
 			
-			// aapt resource value: 0x7f05001d
-			public const int btnSearch = 2131034141;
+			// aapt resource value: 0x7f06001d
+			public const int btnSearch = 2131099677;
 			
-			// aapt resource value: 0x7f05001b
-			public const int btnViewBooking = 2131034139;
+			// aapt resource value: 0x7f06001b
+			public const int btnViewBooking = 2131099675;
 			
-			// aapt resource value: 0x7f050003
-			public const int lblLogin = 2131034115;
+			// aapt resource value: 0x7f060003
+			public const int lblLogin = 2131099651;
 			
-			// aapt resource value: 0x7f050009
-			public const int lblPassword = 2131034121;
+			// aapt resource value: 0x7f060009
+			public const int lblPassword = 2131099657;
 			
-			// aapt resource value: 0x7f050006
-			public const int lblStudentID = 2131034118;
+			// aapt resource value: 0x7f060006
+			public const int lblStudentID = 2131099654;
 			
-			// aapt resource value: 0x7f050002
-			public const int lvClasses = 2131034114;
+			// aapt resource value: 0x7f060002
+			public const int lvClasses = 2131099650;
 			
-			// aapt resource value: 0x7f05000c
-			public const int tableLayout1 = 2131034124;
+			// aapt resource value: 0x7f06000c
+			public const int tableLayout1 = 2131099660;
 			
-			// aapt resource value: 0x7f05000e
-			public const int tableLayout2 = 2131034126;
+			// aapt resource value: 0x7f06000e
+			public const int tableLayout2 = 2131099662;
 			
-			// aapt resource value: 0x7f05000d
-			public const int tableRow1 = 2131034125;
+			// aapt resource value: 0x7f06000d
+			public const int tableRow1 = 2131099661;
 			
-			// aapt resource value: 0x7f050019
-			public const int tableRow2 = 2131034137;
+			// aapt resource value: 0x7f060019
+			public const int tableRow2 = 2131099673;
 			
-			// aapt resource value: 0x7f05001c
-			public const int tableRow3 = 2131034140;
+			// aapt resource value: 0x7f06001c
+			public const int tableRow3 = 2131099676;
 			
-			// aapt resource value: 0x7f05000f
-			public const int tableRow4 = 2131034127;
+			// aapt resource value: 0x7f06000f
+			public const int tableRow4 = 2131099663;
 			
-			// aapt resource value: 0x7f050013
-			public const int tableRow5 = 2131034131;
+			// aapt resource value: 0x7f060013
+			public const int tableRow5 = 2131099667;
 			
-			// aapt resource value: 0x7f050015
-			public const int tableRow6 = 2131034133;
+			// aapt resource value: 0x7f060015
+			public const int tableRow6 = 2131099669;
 			
-			// aapt resource value: 0x7f05001f
-			public const int tableRow7 = 2131034143;
+			// aapt resource value: 0x7f06001f
+			public const int tableRow7 = 2131099679;
 			
-			// aapt resource value: 0x7f05000a
-			public const int tbPassword = 2131034122;
+			// aapt resource value: 0x7f06000a
+			public const int tbPassword = 2131099658;
 			
-			// aapt resource value: 0x7f050007
-			public const int tbStudentID = 2131034119;
+			// aapt resource value: 0x7f060007
+			public const int tbStudentID = 2131099655;
 			
-			// aapt resource value: 0x7f050010
-			public const int textView11 = 2131034128;
+			// aapt resource value: 0x7f060010
+			public const int textView11 = 2131099664;
 			
-			// aapt resource value: 0x7f050014
-			public const int textView12 = 2131034132;
+			// aapt resource value: 0x7f060014
+			public const int textView12 = 2131099668;
 			
-			// aapt resource value: 0x7f050016
-			public const int textView13 = 2131034134;
+			// aapt resource value: 0x7f060016
+			public const int textView13 = 2131099670;
 			
-			// aapt resource value: 0x7f050018
-			public const int textView14 = 2131034136;
+			// aapt resource value: 0x7f060018
+			public const int textView14 = 2131099672;
 			
-			// aapt resource value: 0x7f050011
-			public const int textView15 = 2131034129;
+			// aapt resource value: 0x7f060011
+			public const int textView15 = 2131099665;
 			
-			// aapt resource value: 0x7f050012
-			public const int textView16 = 2131034130;
+			// aapt resource value: 0x7f060012
+			public const int textView16 = 2131099666;
 			
-			// aapt resource value: 0x7f050017
-			public const int textView17 = 2131034135;
+			// aapt resource value: 0x7f060017
+			public const int textView17 = 2131099671;
 			
-			// aapt resource value: 0x7f050004
-			public const int tlLogin = 2131034116;
+			// aapt resource value: 0x7f060004
+			public const int tlLogin = 2131099652;
 			
-			// aapt resource value: 0x7f050008
-			public const int trPassword = 2131034120;
+			// aapt resource value: 0x7f060008
+			public const int trPassword = 2131099656;
 			
-			// aapt resource value: 0x7f050005
-			public const int trStudentID = 2131034117;
+			// aapt resource value: 0x7f060005
+			public const int trStudentID = 2131099653;
 			
 			static Id()
 			{
@@ -212,6 +215,22 @@ namespace HELPSMobileFrontEnd
 			}
 			
 			private String()
+			{
+			}
+		}
+		
+		public partial class Style
+		{
+			
+			// aapt resource value: 0x7f050000
+			public const int Mono_Android_Theme_Splash = 2131034112;
+			
+			static Style()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Style()
 			{
 			}
 		}
