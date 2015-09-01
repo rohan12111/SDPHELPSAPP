bb2cd369f71abaa726e7eb1c29a1c70e319287cd
unknown-user <unknown-user@fe80::247f:8d23:1370:51a8%3>
2015-09-01 16:58:03 +1000

    addition

--- HELPSMobileFrontEnd/Activities/LoginActivity.cs
+++ HELPSMobileFrontEnd/Activities/LoginActivity.cs
@@ -73,3 +73,5 @@ namespace HELPSMobileFrontEnd
 *}
  *
  * }
+ **/
+
--- HELPSMobileFrontEnd/Activities/ViewBookingsActivity.cs
+++ HELPSMobileFrontEnd/Activities/ViewBookingsActivity.cs
@@ -17,11 +17,31 @@ namespace HELPSMobileFrontEnd
 	[Activity (Label = "View Bookings", ScreenOrientation = ScreenOrientation.Portrait)]			
 	public class ViewBookingsActivity : Activity
 	{
+		private List<string> mItems;
+		private ListView mListView;
+
 		protected override void OnCreate (Bundle bundle)
 		{
 			base.OnCreate (bundle);
 
+			SetContentView (Resource.Layout.BookingList);
+			mListView = FindViewById<ListView> (Resource.Id.bookinglist);
+
+
+			mItems = new List<string> ();
+			mItems.Add ("Class1");
+			mItems.Add ("Class2");
+			mItems.Add ("Class3");
+			mItems.Add ("Class4");
+			ArrayAdapter<string> adapter = new ArrayAdapter<string> (this, Android.Resource.Layout.SimpleListItem1, mItems);
+			mListView.Adapter = adapter;
+			mListView.ItemClick += MListView_ItemClick;
 			// Create your application here
+		}
+
+		void MListView_ItemClick (object sender, AdapterView.ItemClickEventArgs e)
+		{
+			
 		}
 	}
 }
