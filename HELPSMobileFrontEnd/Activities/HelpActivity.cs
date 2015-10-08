9c86490f52bde7871b66b4f615b2066b9e32413e
cyonfrost <frost_alone@hotmail.com>
2015-10-08 13:47:21 +1100

    Help page added to the system

--- HELPSMobileFrontEnd/Activities/HelpActivity.cs
+++ HELPSMobileFrontEnd/Activities/HelpActivity.cs
@@ -11,18 +11,46 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.Content.PM;
+using ExpendListBox;
 
 namespace HELPSMobileFrontEnd
 {
-	[Activity (Label = "Help", ScreenOrientation = ScreenOrientation.Portrait)]			
+	[Activity (Label = "HelpActivity", ScreenOrientation = ScreenOrientation.Portrait)]			
 	public class HelpActivity : Activity
 	{
+		Dictionary<string, List<string> > dictGroup = new Dictionary<string, List<string> > ();
+		List<string> lstKeys = new List<string> ();
+
 		protected override void OnCreate (Bundle bundle)
 		{
 			base.OnCreate (bundle);
+			SetContentView (Resource.Layout.HelpPage);
 
-			// Create your application here
+			CreateExpendableListData ();
+
+			var ctlExListBox = FindViewById<ExpandableListView> (Resource.Id.ctlExListBox);
+			ctlExListBox.SetAdapter (new ExpendListAdapter (this, dictGroup));
+
+			ctlExListBox.ChildClick += delegate(object sender, ExpandableListView.ChildClickEventArgs e) {
+				var itmGroup = lstKeys [e.GroupPosition];
+				var itmChild = dictGroup [itmGroup] [e.ChildPosition];
+					
+			};
+
+
+		}
+
+		void CreateExpendableListData ()
+		{
+
+			var lstChild = new List<string> ();
+
+			lstChild.Add (string.Format ("Help : tile klajdsflkajsldkfaj;sdlkfj;ldksja; kldjf ;aklksjlkajdlkajdl;fkajlskdfjal;", 1, 1));
+			dictGroup.Add (string.Format ("What tiles do what", 0), lstChild);
+			dictGroup.Add (string.Format ("How do I make a Booking", 0), lstChild);
+			dictGroup.Add (string.Format ("Frequently asked Question", 0), lstChild);
+
+			lstKeys = new List<string> (dictGroup.Keys);
 		}
 	}
 }
-
---
+++ HELPSMobileFrontEnd/zTabs/ExpandListAdapter.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using HELPSMobileFrontEnd;
+
+
+namespace ExpendListBox
+{
+	class ExpendListAdapter : BaseExpandableListAdapter
+	{
+		Dictionary<string, List<string> > _dictGroup =null;
+		List<string> _lstGroupID = null;
+		Activity _activity;
+
+		public ExpendListAdapter (Activity activity,
+			Dictionary<string, List<string> > dictGroup)
+		{
+			_dictGroup = dictGroup;
+			_activity = activity;
+			_lstGroupID = dictGroup.Keys.ToList();
+
+		}
+		#region implemented abstract members of BaseExpandableListAdapter
+		public override Java.Lang.Object GetChild (int groupPosition, int childPosition)
+		{
+			return _dictGroup [_lstGroupID [groupPosition]] [childPosition];
+		}
+		public override long GetChildId (int groupPosition, int childPosition)
+		{
+			return childPosition;
+		}
+		public override int GetChildrenCount (int groupPosition)
+		{
+			return _dictGroup [_lstGroupID [groupPosition]].Count;
+		}
+		public override View GetChildView (int groupPosition,
+			int childPosition,
+			bool isLastChild,
+			View convertView,
+			ViewGroup parent)
+		{
+			var item = _dictGroup [_lstGroupID [groupPosition]] [childPosition];
+
+			if (convertView == null)
+				convertView = _activity.LayoutInflater.Inflate (Resource.Layout.ListControl_Child, null);
+
+			var textBox = convertView.FindViewById<TextView> (Resource.Id.txtSmall);
+			textBox.SetText (item, TextView.BufferType.Normal);
+
+			return convertView;
+		}
+
+		public override Java.Lang.Object GetGroup (int groupPosition)
+		{
+			return _lstGroupID [groupPosition];
+		}
+		public override long GetGroupId (int groupPosition)
+		{
+			return groupPosition;
+		}
+		public override View GetGroupView (int groupPosition, bool isExpanded, View convertView, ViewGroup parent)
+		{
+			var item = _lstGroupID [groupPosition];
+
+			if (convertView == null)
+				convertView = _activity.LayoutInflater.Inflate (Resource.Layout.ListControl_Group, null);
+
+			var textBox = convertView.FindViewById<TextView> (Resource.Id.txtLarge);
+			textBox.SetText (item, TextView.BufferType.Normal);
+
+			return convertView;
+		}
+		public override bool IsChildSelectable (int groupPosition, int childPosition)
+		{
+			return true;
+		}
+		public override int GroupCount {
+			get {
+				return _dictGroup.Count;
+			}
+		}
+		public override bool HasStableIds {
+			get {
+				return true;
+			}
+		}
+		#endregion
+	}
+}
+
