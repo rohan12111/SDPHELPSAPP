8c700ac24c9cf164d3067045471240ab3da338b0
cyonfrost <frost_alone@hotmail.com>
2015-10-24 19:08:34 +1100

    worked with the image click

--- HELPSMobileFrontEnd/Activities/MainMenuActivity.cs
+++ HELPSMobileFrontEnd/Activities/MainMenuActivity.cs
@@ -34,6 +34,7 @@ namespace HELPSMobileFrontEnd
 				Button btnLogout = FindViewById<Button> (Resource.Id.btnLogout);
 				Button btnMarkAttendance = FindViewById<Button> (Resource.Id.btnMarkAttendance);
 				ImageView ivMakeBooking = FindViewById<ImageView> (Resource.Id.ivMakeBooking);
+				ImageView ivMarkAttendance = FindViewById<ImageView> (Resource.Id.ivMarkAttendance);
 				ImageView ivViewBooking = FindViewById<ImageView> (Resource.Id.ivViewBooking);
 				ImageView ivSearch = FindViewById<ImageView> (Resource.Id.ivSearch);
 				ImageView ivHelp = FindViewById<ImageView> (Resource.Id.ivHelp);
@@ -42,30 +43,49 @@ namespace HELPSMobileFrontEnd
 				btnMakeBooking.Click += delegate  {
 					StartActivity(new Intent(this, typeof(MakeBookingListActivity)));
 				};
+				ivMakeBooking.Click	+=delegate  {
+					StartActivity(new Intent(this, typeof(MakeBookingListActivity)));
+				};
 
 				btnMarkAttendance.Click += delegate {
+					StartActivity(new Intent(this, typeof(CheckInActivity)));
+				};
+				ivMarkAttendance.Click += delegate {
 					StartActivity(new Intent(this, typeof(CheckInActivity)));
 				};
 
 				btnViewBooking.Click += delegate {
 					StartActivity(new Intent(this, typeof(ViewBookingsActivity)));
 				};
+				ivViewBooking.Click += delegate {
+					StartActivity(new Intent(this, typeof(ViewBookingsActivity)));
+				};
 
 				btnSearch.Click += delegate {
+					StartActivity(new Intent(this, typeof(SearchActivity)));
+				};
+				ivSearch.Click += delegate {
 					StartActivity(new Intent(this, typeof(SearchActivity)));
 				};
 
 				btnHelp.Click += delegate {
 					StartActivity(new Intent(this, typeof(HelpActivity)));
 				};
+				ivHelp.Click += delegate {
+					StartActivity(new Intent(this, typeof(HelpActivity)));
+				};
 
 				btnProfile.Click += delegate {
+					StartActivity(new Intent(this, typeof(ProfileActivity)));
+				};
+				ivProfile.Click += delegate {
 					StartActivity(new Intent(this, typeof(ProfileActivity)));
 				};
 
 				btnLogout.Click += delegate {
 					Android.OS.Process.KillProcess(Android.OS.Process.MyPid());
 				};
+
 			}
 			catch (Exception e) 
 			{
